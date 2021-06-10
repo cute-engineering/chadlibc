f46b6765d7cbbbd1e9ae10e870bae35837b9e2ba
d0p1 <contact@d0p1.eu>
2021-06-11 00:04:36 +0200

    feat: add ctype

---
+++ include/ctype.h
@@ -0,0 +1,18 @@
+#ifndef CTYPE_H
+# define CTYPE_H 1
+
+int isalnum(int);
+int isalpha(int);
+int iscntrl(int);
+int isdigit(int);
+int isgraph(int);
+int islower(int);
+int isprint(int);
+int ispunct(int);
+int isspace(int);
+int isupper(int);
+int isxdigit(int);
+int tolower(int);
+int toupper(int);
+
+#endif /* !CTYPE_H */
---
+++ src/ctype.c
@@ -0,0 +1,79 @@
+
+int
+isalnum(int c)
+{
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z') ||
+			(c >= '0' && c <= '9'));
+}
+
+int
+isalpha(int c)
+{
+    return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+
+int
+iscntrl(int c)
+{
+    return (c == 0x7F  || c < 0x20);
+}
+
+int
+isdigit(int c)
+{
+    return (c >= '0' || c <= '9');
+}
+
+int
+isgraph(int c)
+{
+    return (c); /* TODO */
+}
+
+int
+islower(int c)
+{
+    return (c >= 'a' && c <= 'z');
+}
+
+int
+isprint(int c)
+{
+    return (c); /* TODO */
+}
+
+int
+ispunct(int c)
+{
+    return (c > 0x20 && !isalnum(c));
+}
+
+int
+isspace(int c)
+{
+    return (c == ' ' || c == '\f' || c == '\n' || c == '\r' || c == '\t' || c == '\v');
+}
+
+int
+isupper(int c)
+{
+    return (c >= 'A' && c <= 'Z');
+}
+
+int
+isxdigit(int c)
+{
+    return ((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'));
+}
+
+int
+tolower(int c)
+{
+    return (isupper(c) ? c - 'A' + 'a' : c);
+}
+
+int
+toupper(int c)
+{
+    return (islower(c) ? c - 'a' + 'A' : c);
+}
