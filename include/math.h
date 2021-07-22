86d7dc4d7edd5de2b535455b4515be71551ec2c5
Wafelack <wafelack@protonmail.com>
2021-07-22 12:47:29 +0200

    feat: add sqrt

---
+++ include/math.h
@@ -0,0 +1,6 @@
+#ifndef _MATH_H
+# define _MATH_H 1
+
+double sqrt(double);
+
+#endif /* !_MATH_H */
---
+++ src/math.c
@@ -0,0 +1,22 @@
+#include <math.h>
+#include <errno.h>
+#include <stddef.h>
+
+double
+sqrt(double x)
+{
+  double z = 1;
+  size_t i = 0;
+
+  if (x < 0)
+  {
+    errno = EDOM;
+    return 0;
+  }
+
+  for (; i < 10; i++)
+  {
+    z -= (z * z - x) / (2 * z);
+  }
+  return (z);
+}
