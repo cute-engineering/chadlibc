71184c74b68ada2179c89b5bf5cb762338d7c221
Wafelack <wafelack@protonmail.com>
2021-07-24 21:06:53 +0200

    refactor: remove useless variables

--- src/string/memcmp.c
+++ src/string/memcmp.c
@@ -3,18 +3,17 @@
 int
 memcmp(const void *s1, const void *s2, size_t n)
 {
-	size_t i = 0;
 	const char *cs1, *cs2;
 	cs1 = (const char *)s1;
 	cs2 = (const char *)s2;
-	for (; i < n; i++)
+	while (n--)
 	{
-		char first = cs1[i];
-		char second = cs2[i];
-		if (first != second)
+		if (*cs1 != *cs2)
 		{
-			return (first - second);
+			return (*cs1 - *cs2);
 		}
+		cs1++;
+		cs2++;
 	}
 	return (0);
 }
