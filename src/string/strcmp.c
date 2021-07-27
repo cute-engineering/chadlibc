d611273e93ba0e2b93b0dd9ae100a97f7354483e
d0p1 <contact@d0p1.eu>
2021-07-27 13:51:09 +0200

    fix: strcmp wasn't returning proper value

--- src/string/strcmp.c
+++ src/string/strcmp.c
@@ -3,16 +3,11 @@
 int
 strcmp(const char *s1, const char *s2)
 {
- 	size_t i;
-
- 	for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
- 	{
-		if (*s1 != *s2)
-		{
-			return (*s1 - *s2);
-		}
+	while (*s1 && *s1 == *s2)
+	{
 		s1++;
 		s2++;
 	}
-	return (0);
+
+	return (*s1 - *s2);
 }
