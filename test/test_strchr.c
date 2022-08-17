44218bdd1ad063a3ca3b7ecb8498434e3ab72e5a
Keyboard Slayer <dalcjor@libmail.eu>
2022-08-17 21:52:56 +0200

    test: strchr and strrchr

---
+++ test/test_strchr.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+int 
+main(void)
+{
+    int result;
+
+    char *str1 = "Hello,World!";
+    char *str2 = "I love DEVSE";
+    char *str3 = "1;2;3";
+
+    result = strcmp(strchr(str1, ','), ",World!") == 0 ? 0 : 1;
+    result += strcmp(strchr(str2, ' '), " love DEVSE") == 0 ? 0 : 1;
+    result += strcmp(strchr(str3, ';'), ";2;3") == 0 ? 0 : 1;
+
+    return (result);
+}
