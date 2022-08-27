c61ca130d993dbd1d7f419b192bc11fdf6bd8126
lg <abbix@riseup.net>
2022-08-27 10:45:49 -0400

    fix: stop using unistd.h

--- sysdeps/linux/x86_64/generic.c
+++ sysdeps/linux/x86_64/generic.c
@@ -1,8 +1,14 @@
+#include <stdarg.h>
 #include <sys/syscall.h>
-#include <unistd.h>
+
+static void
+syscall1(int nbr, unsigned long arg)
+{
+	__asm__ volatile("syscall" : : "a"(nbr), "D"(arg));
+}
 
 void
 sys_exit(int status)
 {
-	syscall(SYS_exit, status);
+	syscall1(SYS_exit, status);
 }
