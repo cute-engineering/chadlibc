de956ebe196788c064056716410fb611968b777a
d0p1 <contact@d0p1.eu>
2022-12-23 14:15:57 +0100

    chore: add missing license notice headers

--- include/stddef.h
+++ include/stddef.h
@@ -1,18 +1,36 @@
+/*
+ * Copyright 2022 Cute Engineewing 
+ *
+ * This file is part of ChadLibC.
+ *
+ * ChadLibC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ChadLibC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ChadLibC. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _STDDEF_H
 # define _STDDEF_H 1
 
 # define NULL ((void *)0)
 
-#ifdef __SIZE_TYPE__
+# ifdef __SIZE_TYPE__
 typedef __SIZE_TYPE__ size_t;
-#else
+# else
 typedef unsigned long size_t;
-#endif
+# endif /* __SIZE_TYPE__ */
 
-#ifdef __PTRDIFF_TYPE__
+# ifdef __PTRDIFF_TYPE__
 typedef __PTRDIFF_TYPE__ ptrdiff_t;
-#else
+# else
 typedef long ptrdiff_t;
-#endif
+# endif /* __PTRDIFF_TYPE__ */
 
 #endif /* !_STDDEF_H */
--- include/stdlib.h
+++ include/stdlib.h
@@ -1,13 +1,31 @@
+/*
+ * Copyright 2022 Cute Engineewing 
+ *
+ * This file is part of ChadLibC.
+ *
+ * ChadLibC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ChadLibC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ChadLibC. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _STDLIB_H
-#define _STDLIB_H 1
+# define _STDLIB_H 1
 
-#define EXIT_SUCCESS 0
-#define EXIT_FAILURE 1
+# define EXIT_SUCCESS 0
+# define EXIT_FAILURE 1
 
-#define RAND_MAX 32767
+# define RAND_MAX 32767
 
 /* Defined by spec to be **at least** 32 */
-#define ATEXIT_MAX 32
+# define ATEXIT_MAX 32
 
 typedef struct
 {
