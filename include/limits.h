bf3715bda05478b56058ee7c12a6d5fe46c2349b
d0p1 <contact@d0p1.eu>
2022-12-23 14:15:57 +0100

    chore: add missing license notice headers

--- include/limits.h
+++ include/limits.h
@@ -1,23 +1,41 @@
+/*
+ * Copyright 2022 Cute Engineewing 
+ *
+ * This file is part of ChadLibC.
+ *
+ * ChadLibC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ChadLibC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ChadLibC. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _LIMITS_H
-#define _LIMITS_H 1
+# define _LIMITS_H 1
 
-#include <abi/limits.h>
+# include <abi/limits.h>
 
-#define CHAR_BIT   8
-#define CHAR_MAX   SCHAR_MAX
-#define CHAR_MIN   SCHAR_MIN
-#define MB_LEN_MAX 0
-#define INT_MIN	   (-1 - 0x7fffffff)
-#define INT_MAX	   0x7fffffff
-#define LONG_MIN   (-LONG_MAX - 1)
-#define LONG_MAX   __LONG_MAX
-#define SCHAR_MAX  127
-#define SCHAR_MIN  (-128)
-#define SHRT_MAX   0x7fff
-#define SHRT_MIN   (-1 - 0x7fff)
-#define UCHAR_MAX  0xff
-#define UINT_MAX   0xffffffffU
-#define ULONG_MAX  (2UL * LONG_MAX + 1)
-#define USHRT_MAX  0xffff
+# define CHAR_BIT   8
+# define CHAR_MAX   SCHAR_MAX
+# define CHAR_MIN   SCHAR_MIN
+# define MB_LEN_MAX 0
+# define INT_MIN	   (-1 - 0x7fffffff)
+# define INT_MAX	   0x7fffffff
+# define LONG_MIN   (-LONG_MAX - 1)
+# define LONG_MAX   __LONG_MAX
+# define SCHAR_MAX  127
+# define SCHAR_MIN  (-128)
+# define SHRT_MAX   0x7fff
+# define SHRT_MIN   (-1 - 0x7fff)
+# define UCHAR_MAX  0xff
+# define UINT_MAX   0xffffffffU
+# define ULONG_MAX  (2UL * LONG_MAX + 1)
+# define USHRT_MAX  0xffff
 
 #endif /* !_LIMITS_H */
--- include/signal.h
+++ include/signal.h
@@ -1,18 +1,36 @@
+/*
+ * Copyright 2022 Cute Engineewing 
+ *
+ * This file is part of ChadLibC.
+ *
+ * ChadLibC is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free 
+ * Software Foundation, either version 3 of the License, or (at your option)
+ * any later version.
+ *
+ * ChadLibC is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ChadLibC. If not, see <https://www.gnu.org/licenses/>.
+ */
 #ifndef _SIGNAL_H
-#define _SIGNAL_H 1
+# define _SIGNAL_H 1
 
 typedef int sig_atomic_t;
 
-#define SIGABRT 0
-#define SIGFPE	1
-#define SIGILL	2
-#define SIGINT	3
-#define SIGSEGV 4
-#define SIGTERM 5
+# define SIGABRT 0
+# define SIGFPE	1
+# define SIGILL	2
+# define SIGINT	3
+# define SIGSEGV 4
+# define SIGTERM 5
 
-#define SIG_IGN (void (*)(int))0
-#define SIG_DFL (void (*)(int))1
-#define SIG_ERR (void (*)(int))2
+# define SIG_IGN (void (*)(int))0
+# define SIG_DFL (void (*)(int))1
+# define SIG_ERR (void (*)(int))2
 
 void (*signal(int sig, void (*func)(int)))(int);
 int raise(int sig);
